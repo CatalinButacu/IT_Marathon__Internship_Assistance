199c154debc36a365d3bdedd3fdb408978fd4ae7
Pauwul's macbook <petrila.paulcosmin@gmail.com>
2022-04-17 09:55:16 +0300

    small changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -4,53 +4,7 @@
     <div class="status-wrapper">
       <img class="status" src="../assets/status.svg" alt="status" />
     </div>
-    <h3>What is this?</h3>
-    <p class="first-parg">Is a Social Media Application.</p>
-    <p>
-      Social media are interactive computer-mediated technologies that
-      facilitate the creation and sharing of information, ideas, career
-      interests and other forms of expression via virtual communities and
-      networks.
-    </p>
-    <h3>What can You do?</h3>
-    <div class="what-can">
-      <div class="image">
-        <img src="../assets/question.svg" alt="question" />
-      </div>
-      <div class="words">
-        <h4>As a Visitor -</h4>
-        <ul>
-          <li>You can view Posts</li>
-          <li>You can see Comments</li>
-          <li>You can Register and Sign in</li>
-        </ul>
-        <h4>As a Registered, Signed in user -</h4>
-        <ul>
-          <li>You can view Posts</li>
-          <li>You can create new Post</li>
-          <li>You can edit your Posts</li>
-          <li>You can delete your Posts</li>
-          <li>You can see Comments</li>
-          <li>You can write Comments to Posts</li>
-          <li>You can edit your Comments</li>
-          <li>You can delete your Comments</li>
-          <li>
-            You
-            <i>
-              can give Stars
-            </i>
-            to Posts you like
-          </li>
-          <li>
-            You
-            <i>can remove Stars</i> to Posts you unlike
-          </li>
-          <li>You can view users profile</li>
-          <li>You can follow other user you like</li>
-          <li>You can filter posts</li>
-        </ul>
-      </div>
-    </div>
+    
     <h3>Technology</h3>
     <div class="technology">
       <div>
